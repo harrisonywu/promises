c6ff99905617d533a64899cf03d1a59206b05595
harrisonywu <harrisonywu@gmail.com>
2019-03-12 16:01:15 -0700

    implement pluckFirstLineFromFile to spec

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,16 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = data.toString().split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
